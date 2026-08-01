47a2541e2f8bdec2c8160524dd4997a930048e42
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-02 00:12:10 +0100

    Fetch Error

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
+    const [error, setError] = useState(null);
     const [blogs, setBlogs] = useState(null); 
     const [isPending, setIsPending] = useState(true); 
 
@@ -10,19 +11,28 @@ const Home = () => {
         setTimeout (() => {
         fetch("http://localhost:8000/blogs")
         .then (res => {
+            if (!res.ok){
+                throw Error("Could not fetch the data for that resource");
+            }
             return res.json();
         })
         .then(data => {
             console.log(data);
             setBlogs(data);
             setIsPending(false);
-        });
+        })
+        .catch(err =>{
+            setIsPending(false);
+            setError(err.message);
+        })
+        ;
         }, 3000)
     }, []);
 
     return ( 
         <div className="home">
             {isPending && <p>LOADING...</p>}
+            {error && <p>{error}</p>}
             {blogs && <BlogList blogs={blogs} title="ALL BLOGS"/>}
             {blogs && <BlogList title="Anes's BLOGS" blogs={blogs.filter((blog)=>(blog.author === "Anes BOUZEKRI"))} />}
         </div>
