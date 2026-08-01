420422f242aa223a9fff9a31dd132e5c2de500eb
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-01 23:55:48 +0100

    Is Loading Message

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,25 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null); 
+    const [isPending, setIsPending] = useState(true); 
 
     useEffect(() => {
+        setTimeout (() => {
         fetch("http://localhost:8000/blogs")
         .then (res => {
             return res.json();
         })
         .then(data => {
             console.log(data);
-            setBlogs(data); 
+            setBlogs(data);
+            setIsPending(false);
         });
+        }, 3000)
     }, []);
 
     return ( 
         <div className="home">
+            {isPending && <p>LOADING...</p>}
             {blogs && <BlogList blogs={blogs} title="ALL BLOGS"/>}
             {blogs && <BlogList title="Anes's BLOGS" blogs={blogs.filter((blog)=>(blog.author === "Anes BOUZEKRI"))} />}
         </div>
