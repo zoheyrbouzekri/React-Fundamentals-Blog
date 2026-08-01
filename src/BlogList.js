e3d60cfd4ef80c9291e1e5de18ef207f25b64bae
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-01 22:47:18 +0100

    Creat Independant BlogList

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+const BlogList = ({ blogs, title }) => {
+
+    return (
+        <div className="blog-list">
+            <h2>{ title }</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <h3>{ blog.author }</h3>
+                    <p>{ blog.body }</p>
+                </div>
+            ))}
+        </div>
+    );
+
+}
+
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
@@ -8,15 +9,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            <h2>HOME PAGE</h2>
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <h3>{ blog.author }</h3>
-                    <p>{ blog.body }</p>
-                </div>
-            ))}
-            
+            <BlogList blogs={blogs} title="MY BLOGS"/>
         </div>
     );
 }
