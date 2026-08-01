1e87265d661befcffd0a1f921dfe446162110ad5
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-01 21:57:00 +0100

    Ajout de Navbar et Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import Home from './Home';
+import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
+      <Navbar />
+      <header className="content">
+        <Home />
       </header>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h2>HOME PAGE</h2>
+        </div>
+    );
+}
+
+export default Home;
