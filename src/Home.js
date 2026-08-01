18d7f880f67511f6cd84b10eb2364e704f63c781
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-01 22:52:17 +0100

    Adding Anes Blogs

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,14 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState([
-        {title: "First Blog", body:"This blog is the first blog!", author:"Zoheyr BOUZEKRI", id:1}
+        {title: "First Blog", body:"This blog is the first blog!", author:"Zoheyr BOUZEKRI", id:1},
+        {title: "Second Blog", body:"This blog is the second blog!", author:"Anes BOUZEKRI", id:2}
     ]);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="MY BLOGS"/>
+            <BlogList blogs={blogs} title="ALL BLOGS"/>
+            <BlogList title="Anes's BLOGS" blogs={blogs.filter((blog)=>(blog.author === "Anes BOUZEKRI"))} />
         </div>
     );
 }
