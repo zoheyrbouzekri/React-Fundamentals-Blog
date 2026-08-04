5a4c55a37484d7f3abe8db8678d026cbf45b6596
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-04 02:02:32 +0100

    Suppression

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,23 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () => {
+
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        })
+        .then(() => {
+            history.push('/');
+        })
+    }
+
+    const history = useHistory();
+    
     return ( 
         <div className="blog-details">
             <h2>blog details - { id }</h2>
@@ -16,6 +30,7 @@ const BlogDetails = () => {
                         <div>{ blog.body }</div>
                 </article>
             )}
+            <button onClick={handleClick}>delete</button>
         </div>
     );
 }
