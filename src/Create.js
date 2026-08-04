ba786b1b025bf3b3a126c7183ba4b6f69f6b21ec
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-04 01:47:00 +0100

    useHistory

--- src/Create.js
+++ src/Create.js
@@ -1,15 +1,39 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
 
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        
+        e.preventDefault(); // La page ne se rafraîchit plus lors de la soumission
+        const blog = { title, body, author };
+        
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false)
+            // Redirection vers la route racine (Home)
+            history.push("/"); 
+        });
+    };
+    
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                     <input 
                         type="text" 
@@ -31,7 +55,10 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+
+                {/* Affichage conditionnel du bouton selon l'état isPending */}
+                { !isPending && <button>Add Blog</button> }
+                { isPending && <button disabled>Adding blog...</button> }
             </form>
             {/* Affichage des états pour vérifier la synchronisation en direct */}
             <p>{ title }</p>
